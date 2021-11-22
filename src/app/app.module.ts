ae25333c5cf9cf1e9717f55815280790dfa27d12
jeferson-alves <jeferson.online@gmail.com>
2021-11-22 20:15:31 -0300

    component frame1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Frame1Component } from './frame1/frame1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Frame1Component
   ],
   imports: [
     BrowserModule,
