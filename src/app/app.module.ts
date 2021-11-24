245855d5054c15d0fa04ae28a7748effd2f464c6
brunosas88 <bruno.sas@live.com>
2021-11-24 13:13:23 -0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Frame5Component } from './frame5/frame5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Frame5Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/frame5/frame5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frame5',
+  templateUrl: './frame5.component.html',
+  styleUrls: ['./frame5.component.css']
+})
+export class Frame5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
