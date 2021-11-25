0c826687caaaf5fe5a0a87c00fb31e240b1e7c69
brunosas88 <bruno.sas@live.com>
2021-11-24 23:07:27 -0300

    prettier + correção de interação

--- src/app/frame5/frame5.component.ts
+++ src/app/frame5/frame5.component.ts
@@ -3,20 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-frame5',
   templateUrl: './frame5.component.html',
-  styleUrls: ['./frame5.component.css']
+  styleUrls: ['./frame5.component.css'],
 })
 export class Frame5Component implements OnInit {
+  liBold: boolean = false;
+  user: number = 0;
+  userName: string = '';
 
-  liBold: boolean = false
-  user: number = 0
-  userName: string = ''
-
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.user = 1
-    this.userName = 'Usuário 1'
+    this.user = 1;
+    this.userName = 'Usuário 1';
   }
-
 }
