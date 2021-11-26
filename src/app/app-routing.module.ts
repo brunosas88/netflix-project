2f8e3ffa124942eec1410416fec05febbf2b376d
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-25 23:51:37 -0300

    renames frame1 to home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Frame1Component } from './frame1/frame1.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { Frame2Component } from './frame2/frame2.component';
 
 const routes: Routes = [
-	{ path: 'home', component: Frame1Component, },
+	{ path: 'home', component: HomePageComponent },
 	{ path: 'login', component: Frame2Component },
 	// { path: 'user', component: Frame4Component },
-	{ path: '',   redirectTo: 'home', pathMatch: 'full' }
-
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Frame1Component } from './frame1/frame1.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PromoListTextLeftComponent } from './components/promo-list-text-left/promo-list-text-left.component';
 import { PromoListTextRightComponent } from './components/promo-list-text-right/promo-list-text-right.component';
@@ -16,27 +16,26 @@ import { Frame2Component } from './frame2/frame2.component';
 import { FooterLoginComponent } from './frame2/footer-login/footer-login.component';
 import { ToobarLoginComponent } from './frame2/toobar-login/toobar-login.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormLoginComponent,
-    Frame2Component,
+	declarations: [
+		AppComponent,
+		FormLoginComponent,
+		Frame2Component,
 		FooterLoginComponent,
-  	ToobarLoginComponent,
-    Frame1Component,
-    FooterComponent,
-    PromoListTextLeftComponent,
-    PromoListTextRightComponent,
-    FooterStartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
+		ToobarLoginComponent,
+		HomePageComponent,
+		FooterComponent,
+		PromoListTextLeftComponent,
+		PromoListTextRightComponent,
+		FooterStartComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
 		ReactiveFormsModule,
-		HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+		HttpClientModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
