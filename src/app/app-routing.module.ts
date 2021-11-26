4ab181d05f85106e584b6ced4f74dff24ea39082
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-26 17:06:17 -0300

    add series page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SeriesPageComponent } from './series-page/series-page.component';
 
 const routes: Routes = [
 	{ path: 'home', component: HomePageComponent },
 	{ path: 'login', component: LoginPageComponent },
 	// { path: 'user', component: Frame4Component },
+	{ path: 'series', component: SeriesPageComponent },
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
